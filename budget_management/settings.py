4759c2aefd6944199da0823d2ed1e6fde762350c
chanablum1 <c0583275113@gmail.com>
2024-11-13 02:03:09 +0200

    allowed hosts

--- budget_management/settings.py
+++ budget_management/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = 'django-insecure-1n=4tyness3u+b#gh+sbe^&y3j28tp%jl2n+%il$=!&wne0(ia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
