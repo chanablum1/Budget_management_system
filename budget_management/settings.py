16a06a392fb2f314bfc29fca9a688fb1d949b699
chanablum1 <c0583275113@gmail.com>
2025-01-02 23:01:24 +0200

    Adding a React frontend address

--- budget_management/settings.py
+++ budget_management/settings.py
@@ -75,12 +75,16 @@ CSRF_COOKIE_SECURE = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
+    "http://localhost:3000", 
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+
     ]
 
 TEMPLATES = [
